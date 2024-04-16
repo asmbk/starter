01fb0529191ed4086fe06a50f245955c575500fb
Michal Bok <115459085+asmbk@users.noreply.github.com>
2024-04-16 18:32:16 +0200

    italics

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,13 @@ return {
     priority = 1000,
     config = true,
     opts = {
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      },
       transparent_mode = true,
     },
   },
